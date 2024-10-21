036b1e76f7302f82360ffe1dfb6f69d7e96e1aec
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-21 21:59:13 +0200

    9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -2,13 +2,12 @@
  * File 101-quote.c
  */
 
-
 #include <unistd.h>
+
 /**
- * main - display "and that piece of art is useful" -Dora Korpar, 2015-10-19"
+ * main - Prints "and that piece of art is useful" -Dora Korpar, 2015-10-19"
  * Return: always 1
  */
-
 int main(void)
 {
 	write(2, "and that piece of art is useful"\"-Dora Korpar, 2015-10-19\n", 59);
