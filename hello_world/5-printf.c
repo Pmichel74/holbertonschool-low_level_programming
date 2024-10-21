3c545cc023ff6d7f16730b075b3ea4ed2878a564
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-21 15:37:47 +0200

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*  *
+*   * description: Prints a quote using the puts function
+*    * Return: Always 0 (Success)
+ *
+ * Return : always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
