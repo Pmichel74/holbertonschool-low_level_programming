a7742502d9c08d7425c0e59cf7bb2dc5b6a93b6f
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-22 15:23:16 +0200

    06

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+/*
+ *  * File - 5-print_numbers.c
+ *   *
+ *    */
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int num
+for (num = 0; num < 10; num++)
+{
+putchar(num +'0');
+}
+putchar('\n');
+}
+return (0);
