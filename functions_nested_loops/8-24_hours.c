5cd47d4d5788308d1e6b580159a74c2ab0768b35
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-23 18:32:33 +0200

    8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 23; i++)
+	{
+		for (j = 0; j < 59; j++)
+		_putchar((i / 10) + '0');
+		_putchar((i % 10) + '0');
+		_putchar (':');
+		_putchar((i / 10) + '0');
+		_putchar((i % 10) + '0');
+		_putchar('\n')
+	}
+}
