c6d031ee6beafb41704bdf9ee3799039cdd051fa
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-23 09:58:35 +0200

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ * Return: akways 0
+ */
+int void print_alphabet(void)
+{
+	char letter = 'a'
+	While (letter <= 'z')
+		{
+			putchar(letter);
+			c++;
+		}
+	putchar('\n');
+
+	return (0);
+}
+
+
