495f462c2b71e577edb1af3fa7a08d3049a05822
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-23 10:32:25 +0200

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+# include "main"
+/**
+ * comment
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	init i = 0;
+
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+		_putchar (c);
+		c++;
+		}
+		_putchar(\n)
+		c++;
+		}
+}
