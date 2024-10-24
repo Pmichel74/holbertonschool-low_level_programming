1ed36b47a34b457ea93fb4d3a35cc1a82801e665
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-24 15:54:55 +0200

    10

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -22,7 +22,6 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(k + '0');
-				
 			}
 
 			else
@@ -32,9 +31,8 @@ void times_table(void)
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
-			
 		}
- _putchar('\n');
+		_putchar('\n');
 	}
-	
+
 }
