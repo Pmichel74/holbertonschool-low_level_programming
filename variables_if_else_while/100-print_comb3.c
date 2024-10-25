c7ce2deb17c97abcfe5bdc56fde6189b841d86c9
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-25 16:31:47 +0200

    combo3

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,7 @@
  * main - prints all possible different combinations of two digits
  * Return: ALways 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	int i = '0';
 	int j;
@@ -14,13 +14,17 @@ int main (void)
 		while (j <= '9')
 		{
 		if (i < j)
-			putchar('i');
-		        putchar('j');
-			if (i != '8' || i == '8' && j =! '9')
+
+		putchar('i');
+		putchar('j');
+			if (i != '8' || (i == '8' && j != '9'))
+
+			{
 			putchar(',');
 			putchar(' ');
+			}
 		}
-		j++;				
+		j++;			
 	}
 	i++;
 
