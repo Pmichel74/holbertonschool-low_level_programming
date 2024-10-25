b58bd552186dbbaeb9b7f23d05db5714722072d8
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-25 17:02:34 +0200

    combo3

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -5,16 +5,15 @@
  */
 int main(void)
 {
-	int i;
-	int j;
+	int i, j;
 
-	for (i = '0'; i<= '8'; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = i + '1' ; j <= '9'; j++)
+		for (j = i + 1 ; j <= 9; j++)
 		{
 
-		putchar('i');
-		putchar('j');
+		putchar('i'+ '0');
+		putchar('j' + '0');
 			if (i != '8' || j != '9')
 
 			{
