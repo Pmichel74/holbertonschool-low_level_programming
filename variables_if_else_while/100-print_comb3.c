86788cbb4e5b08ada0c4a14dcb4ca37e78ffb2cc
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-26 20:20:29 +0200

    combo3

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -15,12 +15,10 @@ int main(void)
 			putchar(j + '0');
 
 			if (i < 8 || j < 9)
-			{
-			putchar('i'+ '0');
-			putchar('j'+ '0');
+				continue;
+
 			putchar(',');
-			putchar(' ');
-			}			
+			putchar(' ');			
 		}	
 	}
 	putchar('\n');
