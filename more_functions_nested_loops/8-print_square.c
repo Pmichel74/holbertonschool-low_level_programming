294580804412b0bedaf1e10220905cc2e6b5e5cb
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-28 19:21:39 +0100

    9

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,17 +8,20 @@ void print_square(int size)
 {
 	int line;
 	int row;
+
 	 if (size <= 0)
 	 {
 		 _putchar('\n');
 	 }
-
-	for (line = 0; line < size; line++)
-	{
-		for (row = 0; row < size; line++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
-	}
+	 else
+	 {
+		 for (line = 0; line < size; line++)
+		 {
+			 for (row = 0; row < size; row++)
+			 {
+				 _putchar('#');
+			 }
+			 _putchar('\n');
+		 }
+	 }
 }
