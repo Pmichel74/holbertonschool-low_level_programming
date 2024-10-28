1257de6f9cbae07ce29c89528a984d345ffc6c01
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-28 11:05:52 +0100

    2

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main"
+/**
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
+ */
+void print_numbers(void)
+{
+	int digit;
+
+	for (digit = '0'; digit <= '9'; digit++);
+	{
+	_putchar("digit");
+	}
+	_putchar('\n')
+}
