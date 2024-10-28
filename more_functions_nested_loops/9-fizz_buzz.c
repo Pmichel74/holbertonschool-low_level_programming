661a87ca216bb141fb260e9053b0e007194585ea
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-28 20:06:48 +0100

    10

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 	int number;
+	
 	for (number = 1; number <= 100; number++)
 	{
 		if (number % 3 == 0 && number % 5 == 0)
@@ -27,6 +28,10 @@ int main(void)
 		else
 		{
 		printf(" %d", number);
+		}
+		if (number < 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
