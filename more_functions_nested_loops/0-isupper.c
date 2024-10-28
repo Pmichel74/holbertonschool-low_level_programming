a4be0de7a43e9aaddc3e1853c676a8505aa29d44
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-28 10:05:35 +0100

    0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * isupper - checks for uppercase character
+ * _isupper - checks for uppercase character
  * @c: the character to be checked
  * Return: 1 if c is uppercase, 0 otherwise
  */
