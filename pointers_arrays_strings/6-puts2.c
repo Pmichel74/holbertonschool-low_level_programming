9efde17117eed2d3ecb68339abdd0a6f9f8067db
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-30 14:27:48 +0100

    06

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,7 +8,7 @@ void puts2(char *str)
 	int i;
 	int length;
 
-	for (length = 0; str[length] < '\0'; length++)
+	for (length = 0; str[length] != '\0'; length++)
 	{
 	}
 	for (i = 0; i < length; i += 2)
