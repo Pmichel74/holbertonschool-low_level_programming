b9d04ffbba10da1843042b499cf1e40df75d813b
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-30 10:33:08 +0100

    05

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -13,10 +13,13 @@ void rev_string(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 	}
+
+	s = i - 1;
+
 	for (j = 0; j < i / 2; j++)
 		{
 			tmp = s[j];
-			s[j] = s[i - 1 - j];
-			s[i - 1 - j] = tmp;
+			s[j] = s[j];
+			s[j] = tmp;
 		}
 }
