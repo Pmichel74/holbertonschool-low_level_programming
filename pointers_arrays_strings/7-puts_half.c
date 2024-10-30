7e4f0083ae7769011878f71057a77b33b60d30a6
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-30 15:55:13 +0100

    06

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void puts_half(char *str)
+{
+	int i;
+	int length;
+
+	for (length = 0; str[length] != '\0'; length++)
+	{
+	}
+	if (length % 2 == 0)
+	{
+		n = length / 2;
+	_putchar(str[n]);
+	}
+	else
+	{
+		n= (length - 1) / 2 + 1;
+	_putchar(str[n]);
+	}
+}
+	
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,6 +3,8 @@
 
 void reset_to_98(int *n);
 
+void puts_half(char *str);
+
 void puts2(char *str);
 
 void print_rev(char *s);
