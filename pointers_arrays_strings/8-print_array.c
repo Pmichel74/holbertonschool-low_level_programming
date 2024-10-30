7070b414182a650aab62a795b345e1ecd957b9f8
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-30 20:18:59 +0100

    08

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,14 +12,14 @@ void print_array(int *a, int n);
 
 	for (i = 0; i < n; i++)
 	{
-	printf("%d", a[i]);
-	}
-	if (i < n)
-	{
-		printf(", ");
-	}
-	else
-	{
+		if (i == 0)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf(", %d", a[i]);
+		}
 		printf('\n');
 	}
 }
