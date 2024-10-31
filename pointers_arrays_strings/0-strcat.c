4bc7328e09a2e35d9fbfdf6ebae61eba6839385a
Pmichel74 <patrick.yann.michelgmail.com>
2024-10-31 15:59:44 +0100

    00

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -4,8 +4,9 @@
  * _strcat - concatenates two strings
  * @dest: string to append to
  * @src: string to add
+ * Return: a pointer to the resulting string
  */
-char *_strcat(char *dest, char *src)
+char _strcat(char *dest, char *src)
 {
 	int count_dest = 0;
 	int count_src = 0;
