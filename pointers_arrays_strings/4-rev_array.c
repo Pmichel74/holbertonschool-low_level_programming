cd58bcb8c64639891ad50b1443d1fef332763c7b
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-01 20:05:15 +0100

    4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,13 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-    int len_tmp;
-    
-    for (i = 0; i < n / 2; i++)
-    {
+
+	int len_tmp;
+
+	for (i = 0; i < n / 2; i++)
+	{
 		len_tmp = a[i];
-		a[i] = a[n - 1 -i];
-		a[n -1 - i] = len_tmp;
-    }
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = len_tmp;
+	}
 }
