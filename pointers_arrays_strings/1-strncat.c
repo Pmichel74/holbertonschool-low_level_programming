659c1b09af2ea1c6cfff61a271911609f035ca25
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-01 16:43:20 +0100

    1

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,28 +1,29 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates n bytes from a string to another
- * @dest: destination string
- * @src: source string
- * @n: number of bytes of str to concatenate
- *
- * Return: a pointer to the resulting string dest
- */
+* _strncat - concatenates n bytes from a string to another
+* @dest: destination string
+* @src: source string
+* @n: number of bytes of str to concatenate
+*
+* Return: a pointer to the resulting string dest
+*/
 
 char *_strncat(char *dest, char *src, int n)
 {
-    int dest_len = 0;
-    int i = 0;
+int dest_len = 0;
 
-	while (dest[dest_len] != '\0')
+int i = 0;
+
+while (dest[dest_len] != '\0')
 	{
-        dest_len++;
-    }
-	for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-        dest[dest_len + i] = src[i];
-    }
+dest_len++;
+	}
+for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+	dest[dest_len + i] = src[i];
+	}
 	dest[dest_len + i] = '\0';
-    
-    return (dest);
+
+	return (dest);
 }
