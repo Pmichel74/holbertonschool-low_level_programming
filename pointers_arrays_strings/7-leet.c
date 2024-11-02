08cfdaf7ebf821fdcba29671d245f7a482cb8754
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-03 00:13:10 +0100

    7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,28 +1,30 @@
 #include "main.h"
 
 /**
- * leet - encodes a string in 1337
- * @s: string to be encoded
- *
- * Return: the resulting string;
- */
+* leet - encodes a string in 1337
+* @s: string to be encoded
+*
+* Return: the resulting string;
+*/
 char *leet(char *s)
 {
-    int j;
-    int i;
-    char letters[5] = {'a', 'e', 'o', 't', 'l'};
-    char chleet[5] = {'4', '3', '0', '7', '1'};
+	int j;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; j < 5; j++)
-        {
-            if (s[i] == letters[j] || s[i] == letters[j] - 32)
-            {
-                s[i] = chleet[j];
-            }
-        }
-    }
-    return (s);
+	int i;
 
+	char letters[5] = {'a', 'e', 'o', 't', 'l'};
+
+	char chleet[5] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == letters[j] || s[i] == letters[j] - 32)
+			{
+				s[i] = chleet[j];
+			}
+		}
+	}
+	return (s);
 }
