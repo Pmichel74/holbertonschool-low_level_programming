7f30a4ad6c1b0665dce965a917e5dd10c02f2588
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-03 14:18:26 +0100

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,7 +6,30 @@
  */
 int _atoi(char *s)
 {
-    
+    int i = 0;
+    int result = 0;
+    int sign = 0;
+
+        while (s[i] != '\0')
+        {
+            i ++;
+
+            if (s[i] == ' ')
+            {
+                i++;
+            }
+            else if (s[i] == '-' || s[i] == '+')
+            {
+                sign = sign + (s[i] == '-');
+                i++;
+            }
+            for (; s[i] >= '0' && s[i <= '9'; i++])
+            {
+                result = result * 10 + (s[i] - '0');
+                i++
+            }
+            return (0);
+            }
 }
 
 
