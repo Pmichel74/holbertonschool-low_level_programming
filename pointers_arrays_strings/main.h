d987151aac0862b596b92f2234ec56d90970dfe2
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-04 15:23:39 +0100

    2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,6 +5,8 @@ void reset_to_98(int *n);
 
 char *_memset(char *s, char b, unsigned int n);
 
+char *_strchr(char *s, char c);
+
 char *_memcpy(char *dest, char *src, unsigned int n);
 
 char *_strncat(char *dest, char *src, int n);
