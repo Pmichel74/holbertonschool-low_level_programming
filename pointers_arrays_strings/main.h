46e832b974d109dc51404d044c560397a30bcf46
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-04 12:04:55 +0100

    0

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,17 +3,7 @@
 
 void reset_to_98(int *n);
 
-char *leet(char *);
-
-char *cap_string(char *);
-
-char *string_toupper(char *);
-
-void reverse_array(int *a, int n);
-
-int _strcmp(char *s1, char *s2);
-
-char *_strncpy(char *dest, char *src, int n);
+char *_memset(char *s, char b, unsigned int n);
 
 char *_strncat(char *dest, char *src, int n);
 
