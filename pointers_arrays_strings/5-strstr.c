262a5595ff9ae2a86973a07a48e76e6332bbe659
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-05 09:45:47 +0100

    5

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,22 +11,26 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-    int i;
-    int j;
+	int i;
 
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        for (j = 0; needle[j] != '\0'; j++)
-        {
-            if (haystack[i + j] != needle [j])
-            {
-                break; 
-            }
-            if (!needle[j])
-            {
-                return (&haystack[i]);
-            }
-        }
-    }
-    return (NULL);
+	int j;
+
+	if (*needle == '\0')
+	return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle [j])
+			{
+				break;
+			}
+			if (!needle[j])
+			{
+				return (&haystack[i]);
+			}
+		}
+	}
+	return (NULL);
 }
