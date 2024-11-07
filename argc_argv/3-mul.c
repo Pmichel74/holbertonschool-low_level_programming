2a79b69e7551c2a52bdbf8afd27ca946af3746ab
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-07 15:54:39 +0100

    3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * mulmain - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+
+int main (int argc, char *argv[])
+{
+	int number1;
+	int number2;
+	int result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	number1 = atoi(argv[1]);
+	number2 = atoi(argv[2]);
+	result = number1 * number2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
