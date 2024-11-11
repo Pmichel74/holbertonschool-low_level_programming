6e0885f83dac44646c7cc5bf317f2118f55a1860
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-11 11:26:39 +0100

    0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 	return (NULL);
 
-	a = malloc(sizeof(*a) * size);
+	a = (char *) malloc(sizeof(char) * size);
 
 	if (a == NULL)
 	return (0);
