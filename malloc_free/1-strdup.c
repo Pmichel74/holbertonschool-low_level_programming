6af7847571a69b2da301d2e732ddf68be0a54628
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-11 13:07:44 +0100

    1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -12,8 +12,7 @@ char *_strdup(char *str)
 	char *dup;
 	unsigned int length = 0;
 	unsigned int i = 0;
-	
-	
+
 	if (str == NULL)
 	return (NULL);
 
@@ -25,14 +24,15 @@ char *_strdup(char *str)
 	dup = malloc(sizeof(char) * (length + 1));
 
 	if (dup == NULL)
-	return(NULL);
+	return (NULL);
 
 	while (i < length)
 	{
 		dup[i] = str[i];
 		i++;
 	}
-	
-	return(dup);
+
+	return (dup);
 }
+
 
