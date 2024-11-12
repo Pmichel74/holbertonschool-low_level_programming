e5c59f12bfede184df3d57581142ea22a702774c
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-12 10:48:48 +0100

    00

---
+++ more_malloc_free/main.h
@@ -0,0 +1,76 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to_98(int *n);
+
+void *malloc_checked(unsigned int b);
+
+char *_memset(char *s, char b, unsigned int n);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strcat(char *dest, char *src);
+
+int _atoi(char *s);
+
+char *_strcpy(char *dest, char *src);
+
+void print_array(int *a, int n);
+
+void puts_half(char *str);
+
+void puts2(char *str);
+
+void print_rev(char *s);
+
+void rev_string(char *s);
+
+void _puts(char *str);
+
+int _strlen(char *s);
+
+void swap_int(int *a, int *b);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
+
+void times_table(void);
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
+void print_numbers(void);
+
+int _putchar(char c);
+
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void more_numbers(void);
+
+void print_square(int size);
+
+void print_triangle(int size);
+
+#endif
