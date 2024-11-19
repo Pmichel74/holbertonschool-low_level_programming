b16f52614fd9011939fc13346e396cda2fdd3e05
Pmichel74 <patrick.yann.michelgmail.com>
2024-11-19 14:09:52 +0100

    3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -3,28 +3,28 @@
 #include <stdio.h>
 
 /**
- * get_op_func - Sélectionne la fonction correcte pour effectuer l'opération
- * @s: L'opérateur passé en argument
- * Return: Un pointeur vers la fonction qui correspond à l'opérateur
- */
+* get_op_func - Selects the correct function to perform
+*               the operation asked by the user.
+* @s: The operator passed as argument.
+*
+* Return: A pointer to the function corresponding
+*         to the operator given as a parameter.
+*/
 
 int (*get_op_func(char *s))(int, int)
 {
 	 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
 
-	while (ops[i].op != NULL)
-	{
-		if (*(ops)[i].op == *s && s[i] == '\0')
-		return (ops[i].f);
-	}
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	i++;
-	return (0);
+
+	return (ops[i].f);
 }
