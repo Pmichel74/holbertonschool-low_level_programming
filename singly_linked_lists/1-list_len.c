04ee1ed13bb67a09b955efd4cd6a39d6f4602cc2
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-02 18:20:45 +0100

    push 1-list_len

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* print_list - Prints all the elements of a list_t list.
+* @h: The list_t list.
+*
+* Return: The number of nodes.
+*/
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++
+		h = h->next;
+	}
+	return (count);
+}
