c34e6610cdfdd2947146f6d6f7b63cae1b4558d9
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-02 18:28:25 +0100

    fix funtion in h

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
 * list_len - The number of elements in a linked list_t list
@@ -13,7 +12,7 @@ size_t list_len(const list_t *h)
 
 	while (h != NULL)
 	{
-		count++
+		count++;
 		h = h->next;
 	}
 	return (count);
