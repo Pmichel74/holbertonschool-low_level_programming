dfee4177a008931aaef9c80c227d50b88d7187ef
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-03 14:57:47 +0100

    correction of the variable

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,17 +1,21 @@
 #include "lists.h"
+#include <stdlib.h>
+
 /**
  * free_list - Frees a list_t list.
  * @head: A pointer to the list_t list.
  */
 void free_list(list_t *head)
 {
-	list_t *tmpfile;
-	tmpfile = head->next;
+	list_t *tmp;
+
 
 	while (head != NULL)
 	{
-		free (head->str);
-		free (head);
+		tmp = head->next;
+
+		free(head->str);
+		free(head);
 		head = tmp;
 	}
 }
