c9f8207fa4e8212825d29742c2635efb4421f1c1
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-04 17:21:27 +0100

    push 7-insert

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -14,7 +14,6 @@ int sum_dlistint(dlistint_t *head)
 	{
 		sum += head->n;
 		head = head->next;
-
 	}
 	return (sum);
 }
---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ *                            list at a given position.
+ * @h: A pointer to the head of the dlistint_t list.
+ * @idx: The position to insert the new node.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *tmp = *h, *add;
+
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (idx > 0)/*juste avant l'index où inserer nouveau noeud*/
+	idx--;
+	tmp = tmp->next;
+
+	if (!tmp->next)
+	return (add_dnodeint_end(h, n));
+
+	add = malloc(sizeof(dlistint_t));
+	if (!add)
+	return (NULL);
+
+	add->n = n;
+	add->prev = tmp;
+	add->next = tmp->next;
+	tmp->next->prev = add;
+	tmp->next = add;
+
+	return (add);
+}
