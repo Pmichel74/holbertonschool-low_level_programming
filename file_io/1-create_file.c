c902028eb30a6770bfe9893590f0972d66bb7a80
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-06 10:05:58 +0100

    syntaxe error fixed

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -6,7 +6,6 @@
  * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
- *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -17,17 +16,17 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while (text_content[len]);
-		len++;
+		while (text_content[len])
+			len++;
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(0, text_content, len);
 
 	if (o == -1 || w == -1)
-	return (-1);
+		return (-1);
 
-	close (0);
+	close(o);
 
 	return (1);
 }
