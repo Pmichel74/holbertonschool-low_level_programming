d5c3b86683ad23f1407be727f65d8ad8505fbc7c
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-06 10:17:42 +0100

    fix loop

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,13 +15,11 @@ int create_file(const char *filename, char *text_content)
 	return (-1);
 
 	if (text_content)
-	{
-		while (text_content[len])
-			len++;
-	}
+
+	for (len = 0; text_content[len] != 0; len++)
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(0, text_content, len);
+	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
