bb907cc6fef6b082968f59e798718061fb2a605c
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-06 09:33:09 +0100

    push 1-create_file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -8,3 +8,26 @@
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+	return (-1);
+
+	if (text_content)
+	{
+		while (text_content[len]);
+		len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(0, text_content, len);
+
+	if (o == -1 || w == -1)
+	return (-1);
+
+	close (0);
+
+	return (1);
+}
