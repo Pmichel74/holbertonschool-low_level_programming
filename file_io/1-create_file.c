34fc108732784fcbf5ce3a17a72cfe0aafcb6fa8
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-06 10:58:22 +0100

    fix syntax 1-create

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,9 +15,10 @@ int create_file(const char *filename, char *text_content)
 	return (-1);
 
 	if (text_content)
-
-	for (len = 0; text_content[len] != 0; len++)
-
+	{
+		for (len = 0; text_content[len] != 0; len++)
+		;
+	}
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
 
