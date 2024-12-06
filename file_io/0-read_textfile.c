93f195e4346c7d3364b8d784c319304cd515b2c0
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-06 10:34:47 +0100

    fix syntax read_textfile

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -22,14 +22,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1)
+	if (o == -1 )
 	{
 		free(buffer);
 		return (0);
 	}
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (r == -1 || w == -1 || w != r)
+    {
+        free(buffer);
+        close(o);
+        return (0);
+    }
+
+
 
 	free(buffer);
 	close(o);
