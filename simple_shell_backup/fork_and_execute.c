73de5ac0264f610c000dbcd86d935f55e9a65c8f
Pmichel74 <patrick.yann.michelgmail.com>
2024-12-31 13:21:06 +0100

    backup

---
+++ simple_shell_backup/fork_and_execute.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * fork_and_execute - Forks a new process and executes a command
+ * @command_path: Full path to the command to be executed
+ * @args: NULL-terminated array of command arguments
+ * @envp: Array of environment variables
+ * @program_name: Name of the program (usually argv[0])
+ *
+ * Return: 0 on success, 127 if the command is not found, -1 on other failures
+ */
+
+int fork_and_execute(char *command_path, char **args,
+char **envp, char *program_name)
+{
+	pid_t pid;
+	int status;
+
+	if (access(command_path, X_OK) == -1)
+	{
+		fprintf(stderr, "%s: 1: %s: not found\n", program_name, args[0]);
+		return (127);
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error: fork failed");
+		return (-1);
+	}
+	else if (pid == 0)
+	{
+		if (execve(command_path, args, envp) == -1)
+		{
+			perror("Error: execve failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (wait(&status) == -1)
+		{
+			perror("Error: wait failed");
+			return (-1);
+		}
+		if (WIFEXITED(status))
+		{
+			return (WEXITSTATUS(status));
+		}
+	}
+	return (-1);
+}
---
+++ simple_shell_backup/main.h
@@ -0,0 +1,39 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+#define MAX_ARGS 64
+
+
+int main(int argc, char *argv[], char *envp[]);
+
+char *read_command(void);
+
+int fork_and_execute(char *command_path, char **args,
+char **envp, char *program_name);
+
+int execute_command(char **args, char **envp, char *program_name);
+
+char **tokenize_command(char *command);
+void free_tokens(char **tokens);
+
+char *find_path(char *envp[]);
+char *check_absolute_path(char *command);
+char *search_in_path(char *command, char *path);
+char *find_command(char *command, char *envp[]);
+
+void free_args(char **args);
+int lsh_exit(char **args);
+
+
+void print_env(char *envp[]);
+int string_to_int(const char *str, int *result);
+
+#endif
